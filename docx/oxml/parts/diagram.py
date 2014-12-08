95bef935bde56db9baeac5afbd6611eb539c1479
Dan Milon <i@danmilon.me>
2014-12-08 16:02:33 +0200

    diagrams

--- docx/oxml/parts/diagram.py
+++ docx/oxml/parts/diagram.py
@@ -15,43 +15,12 @@ class CT_DataModel(BaseOxmlElement):
 
     @property
     def p_lst(self):
-        return self.xpath('.//a:p')
+        return self.xpath('.//w:p')
 
 
 class CT_P(WORD_CT_P):
     """"""
 
-    pPr = ZeroOrOne('a:pPr')
-    r = ZeroOrMore('a:r')
-
 
 class CT_R(BaseOxmlElement):
     """"""
-
-    rPr = ZeroOrOne('a:rPr')
-    t = ZeroOrMore('a:t')
-    br = ZeroOrMore('a:br')
-    cr = ZeroOrMore('a:cr')
-    tab = ZeroOrMore('a:tab')
-
-    @property
-    def text(self):
-        text = ''
-        for child in self:
-            if child.tag == qn('a:t'):
-                t_text = child.text
-                text += t_text if t_text is not None else ''
-            elif child.tag == qn('a:tab'):
-                text += '\t'
-            elif child.tag in (qn('a:br'), qn('a:cr')):
-                text += '\n'
-        return text
-
-    @text.setter
-    def text(self, text):
-        self.clear_content()
-        _RunContentAppender.append_to_run_from_text(self, text)
-
-    def clear_content(self):
-        for elm in self.xpath('./w:t'):
-            elm.getparent().remove(elm)
--- test.py
+++ test.py
@@ -8,5 +8,5 @@ gdm_parts = [d._document_part.rels[rel_id].target_part for rel_id in dgm_relIds]
 
 
 run_elm = gdm_parts[0].data_model.paragraphs[0].runs[0]._r
-import pdb; pdb.set_trace()
 run_elm.text = 'ff'
+d.save('/home/danmilon/tmp/ff.docx')
